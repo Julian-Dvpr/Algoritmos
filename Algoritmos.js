f7412e5ef4fc59d39005cc03b31de3a846a72efa
Julian Dvpr <87943487+Julian-Dvpr@users.noreply.github.com>
2023-05-17 15:34:08 -0300

    algoritmos

---
+++ Algoritmos.js
@@ -0,0 +1,15 @@
+var twoSum = function (nums, target) {
+
+  
+  for (let index = 0; index < nums.length; index++) {
+    var diff = target - nums[index];
+    //    4       9 -   5
+    var diffIndex = nums.indexOf(diff); // 3
+    //3
+    if (diffIndex !== -1 && diffIndex !== index) {
+      return [index, diffIndex];
+    }
+  }
+};
+
+console.log(twoSum([2, 5, 3, 4, 5, 4, 5], 9));
